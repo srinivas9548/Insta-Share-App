8af4354b780ae01a7432962b350fdcce66c5896f
srinivas9548 <srinivasvasamsetti09@gmail.com>
2024-05-22 12:07:36 +0000

    first commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,81 @@
+import {Component} from 'react'
+
+import {Link, withRouter} from 'react-router-dom'
+
+// import Popup from 'reactjs-popup'
+
 import {IoIosMenu} from 'react-icons/io'
+// import {RiCloseCircleFill} from 'react-icons/ri'
 
-import {Navbar, navItemsContainer} from './styledComponents'
+import {
+  Navbar,
+  NavContent,
+  NavContainer,
+  NavLink,
+  HeaderWebsiteLogoAndTitle,
+  HeaderWebsiteLogo,
+  HeaderWebsiteTitle,
+  HamburgerMenuButton,
+  HamburgerMenuIcon,
+  NavMenuMobile,
+  NavItem,
+  NavItemLink,
+} from './styledComponents'
 
-const Header = () => (
-  <Navbar>
-    <navWebsiteLogo
-      src="https://res.cloudinary.com/srinivasvasamsetti/image/upload/v1716289704/kzuoug08izwrkaajem91.png"
-      alt="website logo"
-    />
-    <navItemsContainer>
-      <IoIosMenu size={30} />
-    </navItemsContainer>
-  </Navbar>
-)
+class Header extends Component {
+  state = {isMenuVisible: false}
 
-export default Header
+  onClickHamburgerMenu = () => {
+    this.setState(prevState => ({
+      isMenuVisible: !prevState.isMenuVisible,
+    }))
+  }
+
+  render() {
+    const {isMenuVisible} = this.state
+    const {match} = this.props
+    console.log(this.props)
+    return (
+      <Navbar>
+        <NavContent>
+          <NavContainer>
+            <NavLink as={Link} to="/">
+              <HeaderWebsiteLogoAndTitle>
+                <HeaderWebsiteLogo
+                  src="https://res.cloudinary.com/srinivasvasamsetti/image/upload/v1716289704/kzuoug08izwrkaajem91.png"
+                  alt="website logo"
+                />
+                <HeaderWebsiteTitle>Insta Share</HeaderWebsiteTitle>
+              </HeaderWebsiteLogoAndTitle>
+            </NavLink>
+            <HamburgerMenuButton
+              type="button"
+              onClick={this.onClickHamburgerMenu}
+            >
+              <HamburgerMenuIcon as={IoIosMenu} />
+            </HamburgerMenuButton>
+          </NavContainer>
+
+          {isMenuVisible && (
+            <NavMenuMobile>
+              <NavItem>
+                <NavItemLink as={Link} selection={match.path === '/'} to="/">
+                  Home
+                </NavItemLink>
+                <NavItemLink
+                  as={Link}
+                  selection={match.path === '/my-profile'}
+                  to="/my-profile"
+                >
+                  Profile
+                </NavItemLink>
+              </NavItem>
+            </NavMenuMobile>
+          )}
+        </NavContent>
+      </Navbar>
+    )
+  }
+}
+
+export default withRouter(Header)
--- src/components/Header/styledComponents.js
+++ src/components/Header/styledComponents.js
@@ -2,11 +2,68 @@ import styled from 'styled-components'
 
 export const Navbar = styled.nav`
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  justify-content: center;
+  border: 1px solid blue;
 `
-export const navItemsContainer = styled.div`
+
+export const NavContent = styled.div`
+  width: 90%;
+`
+
+export const NavContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+`
+
+export const NavLink = styled.link`
+  text-decoration: none;
+`
+
+export const HeaderWebsiteLogoAndTitle = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+export const HeaderWebsiteLogo = styled.img`
+  width: 50px;
+`
+
+export const HeaderWebsiteTitle = styled.h1`
+  font-family: 'Roboto';
+  font-size: 14px;
+  color: #000000;
+  margin-left: 10px;
+`
+
+export const HamburgerMenuButton = styled.button`
+  background-color: transparent;
+  border: none;
+  padding: 0px;
+  cursor: pointer;
+`
+
+export const HamburgerMenuIcon = styled.p`
+  width: 25px;
+  height: 25px;
+`
+
+export const NavMenuMobile = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  list-style-type: none;
+`
+
+export const NavItem = styled.li`
+  margin-right: 10px;
+  color: ${props => (props.selection === true ? '#4094EF' : '#000000')};
+`
+
+export const NavItemLink = styled.link`
+  font-family: 'Roboto';
+  font-size: 16px;
+  color: #000000;
+  font-weight: 700;
+  text-decoration: none;
 `
